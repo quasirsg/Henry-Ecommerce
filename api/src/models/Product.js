3a37dc02b6950ce124af017e9181dea8d1b169fa
Rafael Osorio <rhoa30@gmail.com>
2020-10-06 14:13:56 -0500

    S4: Creaccion de modelo Productos

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,10 +1,30 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('product', {
+
+  sequelize.define("product", {
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    enter_date: {
+      type: DataTypes.DATE,
+    },
+    image: {
       type: DataTypes.STRING,
       allowNull: false,
     },
