9284dc09c7ecb92524a48e046fa84b4273b5c083
Romano Javier <alexis_b13@live.com.ar>
2020-10-07 01:01:19 -0300

    Task completa

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,24 +1,29 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const server = require("express").Router();
+const { Product } = require("../db.js");
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+server.get("/", (req, res, next) => {
+  Product.findAll({})
+    .then((products) => {
+      // console.log(products);
+      return res.status(200).send({ products });
+    })
+    .catch(next);
 });
 
-server.get('/:id', (req, res, next) => {
-	var productId = req.params.id;
-
-	Product.findOne({
-		id:productId
-	})
-		.then(product => {
-			res.send(product);
-		})
-		.catch(next);
+server.get("/:id", (req, res, next) => {
+  var productId = req.params.id;
+  
+  Product.findOne({
+    where: { id: productId },
+  })
+    .then((products) => {
+	  // console.log(products);
+	  if (products === null) {
+		  return res.status(404).send({message:'El proyecto no existe'});
+	  }
+      return res.status(200).send({ products });
+    })
+    .catch(next);
 });
 
 module.exports = server;
