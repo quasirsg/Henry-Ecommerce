ebe948d394f2067496a4582321198d4cc529aff5
Rafael Osorio <rhoa30@gmail.com>
2020-10-07 12:19:32 -0500

    Añadidos comentarios para pasos a seguir

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link,
+} from "react-router-dom";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
-  return (
-    <div></div>
-  );
+  // TODO:Hacer las routes con react-router
+  return <div>Esta Corriendo la App</div>;
 }
 
 export default App;
