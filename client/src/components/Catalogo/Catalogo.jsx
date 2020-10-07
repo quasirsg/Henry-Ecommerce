c37fbb7af08ad311156295d3525996e55052df7f
Rafael Osorio <rhoa30@gmail.com>
2020-10-07 08:22:41 -0500

    S14-T13: Creacion del los archivos

---
+++ client/src/components/Catalogo/Catalogo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Catalogo = () => {
+  return (
+    <div>
+      <div className="catalog">
+        {/* TODO: Crear y llamar componente Categories */}
+        <h2>Categorias</h2>
+        <ul>
+          <li>Categoria 1</li>
+          <li>Categoria 2</li>
+          <li>Categoria 3</li>
+        </ul>
+        <button>Mostrar Todos</button>
+      </div>
+      {/* TODO: Llamar componente productCard */}
+    </div>
+  );
+};
+
+export default Catalogo;
