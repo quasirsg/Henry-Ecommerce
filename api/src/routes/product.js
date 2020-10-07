316d91036ab3e953a2dc307d4bd2e72c5c9aed2b
omar zarate <8omarzarate@gmail.com>
2020-10-07 17:22:03 -0300

    task 25, 26 y 27

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,4 +1,5 @@
 const server = require('express').Router();
+const express = require('express');
 const { Product } = require('../db.js');
 
 server.get('/', (req, res, next) => {
@@ -7,6 +8,46 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+
+server.post('/', (req, res) =>{
+	const {name, stock, description, price, enter_date, image} = req.body;
+	Product.create({
+		name: name,
+		stock: stock,
+		description: description,
+		price: price,
+		enter_date: enter_date,
+		image: image
+	}).then( product=> {
+		res.send(product)
+	})
+});
+
+
+server.put('/:id', (req, res)=>{
+	let productId = req.params.id
+	let update= req.body
+
+	Product.findOne({where: {id: productId}})
+		.then(product=> {
+			product.update(update)
+			.then(productUpdate =>{
+				res.json(productUpdate)
+			})
+		})
+		
+});
+
+
+server.delete('/:id', (req,res)=>{
+	let productId = req.params.id
+	
+		Product.destroy({where:{id: productId}}).then(()=>{
+			res.send('Product deleted')
+		});
+
 });
 
 module.exports = server;
