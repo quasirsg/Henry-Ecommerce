41211eed79bd443925196a2aa3738f48dc20e018
Luciano Valenzuela <lucianooo222@gmail.com>
2020-10-08 12:03:04 -0300

    Se resolvieron conflictos

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,60 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+
+
+server.post('/', (req, res, next) =>{
+	const {name, stock, description, price, enter_date, image} = req.body;
+
+	if (!name || !description || !price || !image)
+		return res.status(400).send('a parameter is missing');
+
+	Product.create({
+		name: name,
+		stock: stock,
+		description: description,
+		price: price,
+		enter_date: enter_date,
+		image: image
+	}).then( product=> {
+		return res.status(200).json(product)
+	}).catch(next)
+});
+
+
+server.put('/:id', (req, res, next)=>{
+	let { id } = req.params
+	let update= req.body
+
+	Product.findOne({where: { id }})
+		.then(product=> {
+			if (!product) {
+				return res.status(404).json({message:'Product doesnt exist'});
+			}
+			product.update(update)
+			.then(productUpdate =>{
+				return res.status(200).json(productUpdate)
+			})
+		})
+		.catch(next)
+		
+});
+
+
+server.delete('/:id', (req,res,next)=>{
+	let { id } = req.params
+	
+	Product.findOne({where:{ id }})
+		.then(product=> {
+			if (!product) {
+				return res.status(404).json({message:'Product doesnt exist'})
+			}
+			product.destroy(product)
+			.then(()=>{
+				return res.status(200).json('Product deleted')
+			});
+		
+		}).catch(next)
+ });
+
 module.exports = server;
