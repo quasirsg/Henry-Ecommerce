deb0d896fdbf2c9175a396d2dd20c247de5abd92
Romano Javier <alexis_b13@live.com.ar>
2020-10-08 17:52:33 -0300

    correcion

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React, { useState, useEffect } from "react";
-import Product from "./components/producto";
+import Product from "./components/productDetail";
+import {Catalogue} from './components/catalogo'
 import { Switch, Route } from "react-router-dom";
+import Axios from "axios";
 
 function App() {
+  // TODO:Hacer las routes con react-router
+  const [product, setProduct] = useState([]);
+  /* ===== Axios Product =====*/
+
+  // let productId = null;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/products/`)
+      .then((res) => {
+        return setProduct(res.data.products);
+      })
+      .catch((err) => {
+        return;
+      });
+  }, []);
 
   return (
     <Switch>
       <Route path="/" exact>
+        <Catalogue props={product} />
       </Route>
-      <Route path="/producto/:id" component={Product} />
+      <Route path="/product/:id" component={Product} />
     </Switch>
   );
 }
