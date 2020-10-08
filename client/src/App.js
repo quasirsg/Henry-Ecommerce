d35a6c7e37092e0534e87c074381ea9da55261dd
fayser17 <lb.lescano.vs@gmail.com>
2020-10-08 16:02:04 -0300

    resolviendo producto por id

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,49 @@
-import React from "react";
-import Product from "./components/produto";
+import React, { useState, useEffect } from "react";
+import Product from "./components/producto";
 import ProductDetail from "./components/productDetail/";
 import Catalogue from "./components/catalogo";
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
-import Product from "./components/produto";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
   // TODO:Hacer las routes con react-router
+  const [product, setProduct] = useState([]);
+  /* ===== Axios Product =====*/
+
+  // let productId = null;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/products/`)
+      .then((res) => {
+        return setProduct(res.data.products);
+      })
+      .catch((err) => {
+        return;
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/products/:id`)
+      .then((res) => {
+        // productId = res.data.products[1];
+        // console.log(productId);
+        return setProduct(res.data.products);
+      })
+      .catch((err) => {
+        return;
+      });
+  }, []);
+
   return (
     <Switch>
-    <Route path="/" exact>
-          Inicio
-          {/* <Catalogo/> */}
-        </Route>
-      <Route exact path="/producto/:id" component={Product} />
+      <Route path="/" exact>
+        <Catalogue props={product} />
+      </Route>
+      <Route exact path={`products/:id`}>
+        <Product props={product} />
+      </Route>
     </Switch>
   );
 }
