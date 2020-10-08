5e2add6dae272c000b26d48bea9190e88c7c4c4b
Rafael Osorio <rhoa30@gmail.com>
2020-10-08 09:42:44 -0500

    S15/30-T5: Creacion modelo categories y borrado carpeta produto

---
+++ api/src/models/Categories.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+
+module.exports = (sequelize) => {
+  // defino el modelo
+
+  sequelize.define("categories", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
