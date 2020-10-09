6b27ed065658724152d55a6aecd9931f3c00f467
Rafael Osorio <rhoa30@gmail.com>
2020-10-08 19:21:39 -0500

    Removido el rating en la productCard

--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Button, Card, CardImg, CardTitle, CardSubtitle } from "reactstrap";
-import "./ProductCard.css";
-import StarRatings from "react-star-ratings";
 import { Link } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
-  // console.log(product);
+  console.log(product);
 
   return (
     // TODO:Create State and link it to the component
@@ -18,12 +17,6 @@ const ProductCard = ({ product }) => {
         alt="Product Card Img"
       />
       <CardTitle>{product.name}</CardTitle>
-      <StarRatings
-        rating={4}
-        starRatedColor="#6B83FC"
-        starDimension="25px"
-        starSpacing="6px"
-      />
       <p>Reviews: {product.review}</p>
       <CardSubtitle>Precio: {product.price}</CardSubtitle>
       <Link to={`/product/${product.id}`}>
