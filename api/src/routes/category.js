f45be11236989352e0d06e303353bcf624e38385
Luciano Valenzuela <lucianooo222@gmail.com>
2020-10-09 12:56:04 -0300

    Se limpiaron rutas

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -5,60 +5,56 @@ server.get('/', (req, res, next) => {
 	Category.findAll()
 		.then(category => {
 
-			if (category === null) return res.status(404).send({message:'No hay categorias'});
-			
-			return res.status(200).send({category});
+			if (!category) return res.status(404).json({ message: 'No hay categorias' });
+
+			return res.status(200).json({ category });
 		})
 		.catch(next);
 });
 
+server.post('/', (req, res, next) => {
+	const { name } = req.body;
 
+	if (!name) return res.status(404).json({ message: 'a parameter is missing' });
 
-server.post('/', (req, res, next) =>{
-	const {name} = req.body;
-
-	if (!name)return res.status(400).send('a parameter is missing');
-
-	Category.create({
-		name: name
-	}).then( category=> {
-		return res.status(200).json(category)
-	}).catch(next)
-});
-
-
-server.put('/:id', (req, res, next)=>{
-	let { id } = req.params
-	let update = req.body
-
-	Category.findOne({where: { id }})
-		.then(category=> {
-			if (!category) {
-				return res.status(404).json({message:'Category doesnt exist'});
-			}
-			category.update(update)
-			.then(categoryUpdate =>{
-				return res.status(200).json(categoryUpdate)
-			})
+	Category.create({ name })
+		.then(category => {
+			return res.status(200).json(category)
 		})
 		.catch(next)
-		
 });
 
 
-server.delete('/:id', (req,res,next)=>{
-	let { id } = req.params
-	
-	Category.findOne({where:{ id }})
-		.then(category=> {
-			if (!category) return res.status(404).json({message:'Category doesnt exist'});
-		
+server.put('/:id', (req, res, next) => {
+	let { id } = req.params;
+	let currentCategory = req.body;
+
+	Category.findOne({ where: { id } })
+		.then(category => {
+			if (!category) return res.status(404).json({ message: 'Category doesnt exist' });
+
+			category.update(currentCategory)
+				.then(categoryUpdate => {
+					return res.status(200).json(categoryUpdate)
+				})
+		})
+		.catch(next)
+});
+
+
+server.delete('/:id', (req, res, next) => {
+	let { id } = req.params;
+
+	Category.findOne({ where: { id } })
+		.then(category => {
+			if (!category) return res.status(404).json({ message: 'Category doesnt exist' });
+
 			category.destroy(category)
-			.then(()=>{
-				return res.status(200).json('Category deleted')
-			});
-		
+				.then(() => {
+					return res.status(200).json({ message: 'Category deleted' })
+				});
+
 		}).catch(next)
- });
+});
 
 module.exports = server;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -2,84 +2,82 @@ const server = require("express").Router();
 const { Product } = require("../db.js");
 
 server.get("/", (req, res, next) => {
-  Product.findAll({})
-    .then((products) => {
-      // console.log(products);
-      return res.status(200).send({ products });
-    })
-    .catch(next);
+	Product.findAll({})
+		.then((products) => {
+			return res.status(200).json({ products });
+		})
+		.catch(next);
 });
 
 server.get("/:id", (req, res, next) => {
-  var productId = req.params.id;
-  
-  Product.findOne({
-    where: { id: productId },
-  })
-    .then((products) => {
-	  // console.log(products);
-	  if (products === null) {
-		  return res.status(404).send({message:'El producto no existe'});
-	  }
-      return res.status(200).send({ products });
-    })
-    .catch(next);
+	var { id } = req.params;
+
+	Product.findOne({ where: { id } })
+		.then((products) => {
+
+			if (!products) return res.status(404).json({ message: 'El producto no existe' });
+
+			return res.status(200).json({ products });
+		})
+		.catch(next);
 });
 
-
-
-server.post('/', (req, res, next) =>{
-	const {name, stock, description, price, enter_date, image} = req.body;
+server.post('/', (req, res, next) => {
+	const { name, stock, description, price, image } = req.body;
 
 	if (!name || !description || !price || !image)
-		return res.status(400).send('a parameter is missing');
+		return res.status(400).json({ message: 'A parameter is missing' });
 
 	Product.create({
-		name: name,
-		stock: stock,
-		description: description,
-		price: price,
-		enter_date: enter_date,
-		image: image
-	}).then( product=> {
-		return res.status(200).json(product)
-	}).catch(next)
-});
-
-
-server.put('/:id', (req, res, next)=>{
-	let { id } = req.params
-	let update= req.body
-
-	Product.findOne({where: { id }})
-		.then(product=> {
-			if (!product) {
-				return res.status(404).json({message:'Product doesnt exist'});
-			}
-			product.update(update)
-			.then(productUpdate =>{
-				return res.status(200).json(productUpdate)
-			})
+		name,
+		stock,
+		description,
+		price,
+		image
+	})
+		.then(product => {
+			return res.status(200).json(product)
 		})
 		.catch(next)
-		
 });
 
+server.put('/:id', (req, res, next) => {
+	let { id } = req.params;
+	let update = req.body;
 
-server.delete('/:id', (req,res,next)=>{
-	let { id } = req.params
-	
-	Product.findOne({where:{ id }})
-		.then(product=> {
-			if (!product) {
-				return res.status(404).json({message:'Product doesnt exist'})
-			}
+	Product.findOne({ where: { id } })
+		.then(product => {
+
+			if (!product) return res.status(404).json({ message: 'Product doesnt exist' });
+
+			product.update(update)
+				.then(productUpdate => {
+					return res.status(200).json(productUpdate)
+				})
+		})
+		.catch(next)
+});
+
+server.delete('/:id', (req, res, next) => {
+	let { id } = req.params;
+
+	Product.findOne({ where: { id } })
+		.then(product => {
+
+			if (!product) return res.status(404).json({ message: 'Product doesnt exist' })
+
 			product.destroy(product)
-			.then(()=>{
-				return res.status(200).json('Product deleted')
-			});
-		
+				.then(() => {
+					return res.status(200).json({ message: 'Product deleted' })
+				})
+
 		}).catch(next)
- });
+});
+
+server.get('/categoria/:nombreCat', (req, res) => {
+	const { nombreCat } = req.params;
+
+	
+})
 
 module.exports = server;
