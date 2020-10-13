926b55a536351658c050505a1348034bb9e1ca64
Rafael Osorio <rhoa30@gmail.com>
2020-10-13 10:30:23 -0500

    Correccion nombres en los import que evitaban el buen funcionamiento

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import axios from "axios";
 
 //Components
-import Navbar from './components/navbar'
+import Navbar from "./components/navbar";
 import ProductDetail from "./components/productDetail";
-import Catalogue from "./components/Catalogo";
+import Catalogue from "./components/catalogo";
 import FormProduct from "./components/productForm";
-import FormCategory from './components/categoryForm';
+import FormCategory from "./components/categoryForm";
 
 //Pages
-import SearchPage from './pages/SearchPage';
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   const [product, setProduct] = useState([]);
@@ -42,49 +42,56 @@ function App() {
       <Switch>
         <Route path="/search/q/:searchTerm" component={SearchPage} />
         <Route exact path="/products">
-          <Catalogue
-            products={product}
-            category={category}
-          />
+          <Catalogue products={product} category={category} />
         </Route>
         <Route exact path="/product/:id">
-          <ProductDetail
-            props={product}
-          />
+          <ProductDetail props={product} />
         </Route>
-        <Route exact path="/admin/product/add" render={() => (
-          <FormProduct
-            action="post"
-            icon="success"
-            message="Se agregó producto:"
-            category={category}
-          />
-        )}
+        <Route
+          exact
+          path="/admin/product/add"
+          render={() => (
+            <FormProduct
+              action="post"
+              icon="success"
+              message="Se agregó producto:"
+              category={category}
+            />
+          )}
         />
-        <Route exact path="/admin/product/edit/:productId" render={() => (
-          <FormProduct
-            action="post"
-            icon="success"
-            message="Se edito el producto:"
-            category={category}
-          />
-        )}
+        <Route
+          exact
+          path="/admin/product/edit/:productId"
+          render={() => (
+            <FormProduct
+              action="post"
+              icon="success"
+              message="Se edito el producto:"
+              category={category}
+            />
+          )}
         />
-        <Route exact path='/admin/category/add' render={() =>
-          <FormCategory
-            action='post'
-            icon='success'
-            message='La categoria fue creada:'
-          />
-        }
+        <Route
+          exact
+          path="/admin/category/add"
+          render={() => (
+            <FormCategory
+              action="post"
+              icon="success"
+              message="La categoria fue creada:"
+            />
+          )}
         />
-        <Route exact path='/admin/category/edit/:categoryId' render={() =>
-          <FormCategory
-            action='post'
-            icon='success'
-            message='La categoria fue editada:'
-          />
-        }
+        <Route
+          exact
+          path="/admin/category/edit/:categoryId"
+          render={() => (
+            <FormCategory
+              action="post"
+              icon="success"
+              message="La categoria fue editada:"
+            />
+          )}
         />
       </Switch>
     </div>
