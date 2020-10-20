884c6013aeab4f6f93852f6c4110bec7e7a2e8a6
fayser17 <lb.lescano.vs@gmail.com>
2020-10-20 10:49:48 -0300

    modificaciones

--- client/src/components/shoppingCart/index.jsx
+++ client/src/components/shoppingCart/index.jsx
@@ -3,14 +3,16 @@ import { Col, Row } from "reactstrap";
 import ItemCart from "./itemCart";
 
 const ShoppingCart = ({ items }) => {
+  console.log(items);
 
   return (
     <Col lg="12">
       <h6>Item(s)</h6>
       <Row>
-        {items.map((item) => (
-          <ItemCart product={item.product} quantity={item.quantity} />
-        ))}
+        {items.map((item) => {
+          console.log(item);
+          return <ItemCart product={item} quantity={item} />;
+        })}
       </Row>
     </Col>
   );
--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,32 +1,25 @@
-import React from 'react'
-
-import { Container, Col, Row } from 'reactstrap'
-import ShoppingCart from '../components/shoppingCart'
-import ButtonBlock from '../components/custom/ButtonBlock'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Container, Col, Row } from "reactstrap";
+import ShoppingCart from "../components/shoppingCart";
+import ButtonBlock from "../components/custom/ButtonBlock";
+import { useSelector } from "react-redux";
 const Cart = () => {
-
-    const productsCarts = useSelector(state => state.cart.products);
-    return (
-        <Container
-            fluid={true}
-            className="mt-4"
-        >
-            <Row>
-                <Col lg="8">
-                    <ShoppingCart
-                        items={productsCarts}
-                    />
-                </Col>
-                <Col lg="4">
-                    DETALLES DEL CLIENTE , METODOS DE PAGO Y BOTON PARA PROCEDER AL CHECKOUT
-                    <ButtonBlock
-                        children={'Siguiente'}
-                    />
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+  const productsCarts = useSelector((state) => state.cart.products);
+  console.log(productsCarts);
+  return (
+    <Container fluid={true} className="mt-4">
+      <Row>
+        <Col lg="8">
+          <ShoppingCart items={productsCarts} />
+        </Col>
+        <Col lg="4">
+          DETALLES DEL CLIENTE , METODOS DE PAGO Y BOTON PARA PROCEDER AL
+          CHECKOUT
+          <ButtonBlock children={"Siguiente"} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Cart;
