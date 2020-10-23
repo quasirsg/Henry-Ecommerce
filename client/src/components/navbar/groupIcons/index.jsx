f4eda5a184f0c559973b892e329628ac45bc64ac
fayser17 <lb.lescano.vs@gmail.com>
2020-10-23 10:35:04 -0300

    carrito count

--- client/src/components/navbar/groupIcons/index.jsx
+++ client/src/components/navbar/groupIcons/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { Cart3, Collection } from "react-bootstrap-icons";
 import { Col, CustomInput, Badge } from "reactstrap";
 //Components
 import Guest from "../../guestOptions";
+import { useSelector } from "react-redux";
 
 export default () => {
-
-  const cart = useSelector(state => state.users.carrito);
+  const cart = useSelector((state) => state.users.carrito);
 
   return (
     <Col lg="2" style={{ display: "flex", justifyContent: "space-between" }}>
@@ -17,10 +17,23 @@ export default () => {
       </Link>
       <Link to={"/cart"} className="text-dark">
         <Cart3 size={20} />
-        { 
-          cart.length > 0 && <Badge color="danger" style={{paddingTop: '2px', paddingBottom: '2px'}} className='rounded-pill' size={5}> {cart.length} </Badge>
-        }
+        {cart.length > 0 && (
+          <Badge
+            color="danger"
+            style={{ paddingTop: "2px", paddingBottom: "2px" }}
+            className="rounded-pill"
+            size={5}
+          >
+            {" "}
+            {cart.length}{" "}
+          </Badge>
+        )}
       </Link>
+      {count.length > 0 ? (
+        <Badge href="#" color="danger">
+          {count.length}
+        </Badge>
+      ) : null}
 
       <Guest />
 
